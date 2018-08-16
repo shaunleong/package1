6963e702422bba3fb4ca9b77b0435d9933d40a98
shaunleong <35859746+shaunleong@users.noreply.github.com>
2018-08-16 10:09:00 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -6,17 +6,17 @@ enum stateUnit {
 }
 
 /**
- * Toggle an LED
+ * Flash an LED
  */
 //% color="#2c3e50" weight=10
 namespace led {
     /**
-     * Toggle an LED
+     * Flash an LED
      * @param led_pin LED pin
      * @param state on or off
      */
-    //% blockId=led_toggle="led toggle led_pin %led_pin|state %state"
-    export function toggle(led_pin: AnalogPin, state: stateUnit): void {
+    //% blockId=led_flash="led flash led_pin %led_pin|state %state"
+    export function flash(led_pin: AnalogPin, state: stateUnit): void {
 		pins.digitalWritePin(led_pin, state);
 		control.waitMicros(1000);
 		pins.digitalWritePin(led_pin, !state);
