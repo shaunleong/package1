2a89568e6401136b806a381cf914476c7aaed974
shaunleong <35859746+shaunleong@users.noreply.github.com>
2018-08-16 10:06:30 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,41 +1,24 @@
 enum PingUnit {
-    //% block="μs"
-    MicroSeconds,
-    //% block="cm"
-    Centimeters,
-    //% block="inches"
-    Inches
+    //% block="on"
+	On,
+	//% block="off"
+	Off,
 }
 
 /**
  * Sonar and ping utilities
  */
 //% color="#2c3e50" weight=10
-namespace sonar {
+namespace led {
     /**
-     * Send a ping and get the echo time (in microseconds) as a result
-     * @param trig tigger pin
-     * @param echo echo pin
-     * @param unit desired conversion unit
-     * @param maxCmDistance maximum distance in centimeters (default is 500)
+     * Toggle an LED
+     * @param led_pin LED pin
+     * @param state on or off
      */
-    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
-    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
-        // send pulse
-        pins.setPull(trig, PinPullMode.PullNone);
-        pins.digitalWritePin(trig, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(trig, 1);
-        control.waitMicros(10);
-        pins.digitalWritePin(trig, 0);
-
-        // read pulse
-        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
-
-        switch (unit) {
-            case PingUnit.Centimeters: return Math.idiv(d, 58);
-            case PingUnit.Inches: return Math.idiv(d, 148);
-            default: return d ;
-        }
+    //% blockId=led_toggle="led toggle led_pin %led_pin|state %state"
+    export function toggle(led_pin: AnalogPin, state: boolean): void {
+		pins.digitalWritePin(led_pin, state);
+		control.waitMicros(1000);
+		pins.digitalWritePin(led_pin, !state);
     }
 }
